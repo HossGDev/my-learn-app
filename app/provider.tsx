864169b8b140907b123d0f3e743c2ac5ee945ebc
Hoss Gassaway <stuckinasimulation@protonmail.com>
2026-01-06 14:52:32 -0700

    feat(db): setup initial database schema and persistence layer

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,33 @@
 "use client";
+import { useUser } from "@clerk/nextjs";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 function AppThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const { user } = useUser();
+  const [userDetail, setUserDetail] = useState();
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user]);
+
+  const CreateNewUser = async () => {
+    const result = await axios.post("/api/user", {});
+    console.log(result);
+    setUserDetail(result?.data);
+  };
+  return (
+    <NextThemesProvider {...props}>
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        {children}
+      </UserDetailContext.Provider>
+    </NextThemesProvider>
+  );
 }
 
 export default AppThemeProvider;
